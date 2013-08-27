76de81c74d61d7f4c0a91b902b0f7a697f97d62b
nclavaud <nclavaud@gmail.com>
2013-08-27 23:38:11 +0200

    Set indentation to 2 spaces

--- src/picking/Picker.java
+++ src/picking/Picker.java
@@ -30,102 +30,102 @@ package picking;
 import processing.core.*;
 
 public class Picker {
-    public final static String VERSION = "##library.prettyVersion##";
+  public final static String VERSION = "##library.prettyVersion##";
 
-    /**
-     * Processing applet
-     */
-    protected PApplet parent;
+  /**
+   * Processing applet
+   */
+  protected PApplet parent;
 
-    /**
-     * Main picking buffer Direct access to the buffer is allowed so you can
-     * draw shapes that you wouldn't like to draw on the main screen (like
-     * bounding boxes).
-     */
-    public Buffer buffer;
+  /**
+   * Main picking buffer Direct access to the buffer is allowed so you can
+   * draw shapes that you wouldn't like to draw on the main screen (like
+   * bounding boxes).
+   */
+  public Buffer buffer;
 
-    public Picker(PApplet parent) {
-        this.parent = parent;
-        buffer = (Buffer) parent.createGraphics(parent.width, parent.height, "picking.Buffer");
-        buffer.callCheckSettings();
-        buffer.background(0);
+  public Picker(PApplet parent) {
+    this.parent = parent;
+    buffer = (Buffer) parent.createGraphics(parent.width, parent.height, "picking.Buffer");
+    buffer.callCheckSettings();
+    buffer.background(0);
 
-        parent.registerMethod("pre", this);
-        parent.registerMethod("draw", this);
-        welcome();
+    parent.registerMethod("pre", this);
+    parent.registerMethod("draw", this);
+    welcome();
+  }
+
+  public void pre() {
+    parent.beginRecord(buffer);
+  }
+
+  public void draw() {
+    // make sure recorder is there before shutting it down
+    if (parent.recorder == null) {
+      parent.recorder = buffer;
+    }
+    parent.endRecord();
+  }
+
+  /**
+   * Begins recording object(s)
+   * 
+   * @param i Object ID
+   */
+  public void start(int i) {
+    if (i < 0 || i > 16777214) {
+      PApplet.println("[Picking error] start(): ID out of range");
+      return;
     }
 
-    public void pre() {
-        parent.beginRecord(buffer);
+    if (parent.recorder == null) {
+      parent.recorder = buffer;
     }
+    buffer.setCurrentId(i);
+  }
 
-    public void draw() {
-        // make sure recorder is there before shutting it down
-        if (parent.recorder == null) {
-            parent.recorder = buffer;
-        }
-        parent.endRecord();
+  /**
+   * Stops/pauses recording object(s)
+   */
+  public void stop() {
+    parent.recorder = null;
+  }
+
+  /**
+   * Resumes recording object(s)
+   */
+  public void resume() {
+    if (parent.recorder == null) {
+      parent.recorder = buffer;
     }
+  }
 
-    /**
-     * Begins recording object(s)
-     * 
-     * @param i Object ID
-     */
-    public void start(int i) {
-        if (i < 0 || i > 16777214) {
-            PApplet.println("[Picking error] start(): ID out of range");
-            return;
-        }
+  /**
+   * Reads the ID of the object at point (x, y) -1 means there is no object at
+   * this point
+   * 
+   * @param x X coordinate
+   * @param y Y coordinate
+   * @return Object ID
+   */
+  public int get(int x, int y) {
+    return buffer.getId(x, y);
+  }
 
-        if (parent.recorder == null) {
-            parent.recorder = buffer;
-        }
-        buffer.setCurrentId(i);
-    }
+  /**
+   * Get the buffer
+   * 
+   * @return Buffer
+   */
+  public PGraphics getBuffer() {
+    return buffer;
+  }
 
-    /**
-     * Stops/pauses recording object(s)
-     */
-    public void stop() {
-        parent.recorder = null;
-    }
+  public static String version() {
+    return VERSION;
+  }
 
-    /**
-     * Resumes recording object(s)
-     */
-    public void resume() {
-        if (parent.recorder == null) {
-            parent.recorder = buffer;
-        }
-    }
-
-    /**
-     * Reads the ID of the object at point (x, y) -1 means there is no object at
-     * this point
-     * 
-     * @param x X coordinate
-     * @param y Y coordinate
-     * @return Object ID
-     */
-    public int get(int x, int y) {
-        return buffer.getId(x, y);
-    }
-
-    /**
-     * Get the buffer
-     * 
-     * @return Buffer
-     */
-    public PGraphics getBuffer() {
-        return buffer;
-    }
-
-    public static String version() {
-        return VERSION;
-    }
-
-    private void welcome() {
-        System.out.println("##library.name## ##library.prettyVersion## by ##author##");
-    }
+  private void welcome() {
+    System.out.println("##library.name## ##library.prettyVersion## by ##author##");
+  }
 }
