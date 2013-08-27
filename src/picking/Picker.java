de7c689899cd29fd55f84a17af8ce59eb6b5e6e2
nclavaud <nclavaud@gmail.com>
2013-08-27 22:08:25 +0200

    Fix indentation and tabs

--- src/picking/Picker.java
+++ src/picking/Picker.java
@@ -30,100 +30,106 @@ package picking;
 import processing.core.*;
 
 public class Picker {
-	public final static String VERSION = "##library.prettyVersion##";
-	
+    public final static String VERSION = "##library.prettyVersion##";
+
     /**
      * Processing applet
      */
     protected PApplet parent;
-    
+
     /**
-     * Main picking buffer
-     * Direct access to the buffer is allowed so you can draw shapes that
-     * you wouldn't like to draw on the main screen (like bounding boxes).
+     * Main picking buffer Direct access to the buffer is allowed so you can
+     * draw shapes that you wouldn't like to draw on the main screen (like
+     * bounding boxes).
      */
     public Buffer buffer;
-    
+
     public Picker(PApplet parent) {
-            this.parent = parent;
-            buffer = (Buffer) parent.createGraphics(parent.width, parent.height, "picking.Buffer");
-            buffer.callCheckSettings();
-            if (parent.recorder == null) {
-                    parent.recorder = buffer;
-            }
-            buffer.background(0);
-            parent.registerMethod("pre", this);
-            parent.registerMethod("draw", this);
-            welcome();
+        this.parent = parent;
+        buffer = (Buffer) parent.createGraphics(parent.width, parent.height,
+                "picking.Buffer");
+        buffer.callCheckSettings();
+        if (parent.recorder == null) {
+            parent.recorder = buffer;
+        }
+        buffer.background(0);
+        parent.registerMethod("pre", this);
+        parent.registerMethod("draw", this);
+        welcome();
     }
-    
+
     public void pre() {
-            buffer.beginDraw();
-            if (parent.recorder == null) {
-                    parent.recorder = buffer;
-            }
+        buffer.beginDraw();
+        if (parent.recorder == null) {
+            parent.recorder = buffer;
+        }
     }
-    
+
     public void draw() {
-            buffer.endDraw();
+        buffer.endDraw();
     }
-    
+
     /**
      * Begins recording object(s)
      * 
      * @param i Object ID
      */
     public void start(int i) {
-            if (i < 0 || i > 16777214) {
-                    PApplet.println("[Picking error] start(): ID out of range");
-                    return;
-            }
-            if (parent.recorder == null) {
-                    parent.recorder = buffer;
-            }
-            buffer.setCurrentId(i);
+        if (i < 0 || i > 16777214) {
+            PApplet.println("[Picking error] start(): ID out of range");
+            return;
+        }
+        if (parent.recorder == null) {
+            parent.recorder = buffer;
+        }
+        buffer.setCurrentId(i);
     }
-    
+
     /**
      * Stops/pauses recording object(s)
      */
     public void stop() {
-            parent.recorder = null;
+        parent.recorder = null;
     }
-    
+
     /**
      * Resumes recording object(s)
      */
     public void resume() {
-            if (parent.recorder == null) {
-                    parent.recorder = buffer;
-            }
+        if (parent.recorder == null) {
+            parent.recorder = buffer;
+        }
     }
-    
+
     /**
-     * Reads the ID of the object at point (x, y)
-     * -1 means there is no object at this point
-     * @param x X coordinate
-     * @param y Y coordinate
+     * Reads the ID of the object at point (x, y) -1 means there is no object at
+     * this point
+     * 
+     * @param x
+     *            X coordinate
+     * @param y
+     *            Y coordinate
      * @return Object ID
      */
     public int get(int x, int y) {
-            return buffer.getId(x, y);
+        return buffer.getId(x, y);
     }
-    
+
     /**
      * Get the buffer
+     * 
      * @return Buffer
      */
     public PGraphics getBuffer() {
-            return buffer;
+        return buffer;
     }
 
-	public static String version() {
-		return VERSION;
-	}
+    public static String version() {
+        return VERSION;
+    }
 
-	private void welcome() {
-		System.out.println("##library.name## ##library.prettyVersion## by ##author##");
-	}
+    private void welcome() {
+        System.out
+                .println("##library.name## ##library.prettyVersion## by ##author##");
+    }
 }
