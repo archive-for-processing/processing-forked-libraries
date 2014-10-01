13ed0c6d195a15bd621b957ebad998374874188c
TheMaxJ <maxjohnson@Maxs-MacBook-Air-3.local>
2014-09-30 22:27:33 -0700

    Created Chart Interface

---
+++ src/me/max/chartly/charts/Chart.java
@@ -0,0 +1,13 @@
+package me.max.chartly.charts;
+
+import java.util.HashMap;
+
+public interface Chart {
+
+	public void draw(float x, float y);
+	public void refresh();
+	
+	public void setData(String[] keys, Float[] values);
+	public HashMap<String, Double> getData();
+
+}
