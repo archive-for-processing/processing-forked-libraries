9f0d6f6be60f089a0d8b2ee4ed4cf28f0ef4a2a6
TheMaxJ <maxjohnson@Maxs-MacBook-Air-3.local>
2014-10-15 16:16:25 -0700

    Added KeyValueAmount exception

--- src/me/max/chartly/exceptions/InputException.java
+++ src/me/max/chartly/exceptions/InputException.java
@@ -3,5 +3,10 @@ package me.max.chartly.exceptions;
 public abstract class InputException extends Exception {
 
 	private static final long serialVersionUID = 1L; //no idea what this does.
-
+	
+	public InputException() { super(); }
+	public InputException(String message) { super(message); }
+	public InputException(String message, Throwable cause) { super(message, cause); }
+	public InputException(Throwable cause) { super(cause); }
+	
 }
---
+++ src/me/max/chartly/exceptions/KeyValueAmountException.java
@@ -0,0 +1,12 @@
+package me.max.chartly.exceptions;
+
+public class KeyValueAmountException extends InputException {
+
+	private static final long serialVersionUID = -4181974239466042064L; //auto-generated
+	
+	public KeyValueAmountException() { super(); }
+	public KeyValueAmountException(String message) { super(message); }
+	public KeyValueAmountException(String message, Throwable cause) { super(message, cause); }
+	public KeyValueAmountException(Throwable cause) { super(cause); }
+
+}
