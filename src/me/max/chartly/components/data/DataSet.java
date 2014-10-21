d347d34dd8067fcb32d67da4f534e6ac00ab7e69
TheMaxJ <maxjohnson@Maxs-MacBook-Air-3.local>
2014-10-21 11:20:35 -0700

    Better Exception Handling

--- src/me/max/chartly/components/data/DataSet.java
+++ src/me/max/chartly/components/data/DataSet.java
@@ -3,9 +3,15 @@ package me.max.chartly.components.data;
 import java.util.ArrayList;
 
 import me.max.chartly.charts.BarChart;
+import me.max.chartly.charts.LineChart;
 import me.max.chartly.charts.PieChart;
+import me.max.chartly.exceptions.ExceptionWriter;
 import me.max.chartly.exceptions.KeyValueAmountException;
 
+/**
+ * Used to contain sets of data to be given to charts
+ * @author maxjohnson
+ */
 public class DataSet {
 	
 	public ArrayList<DataPair> data;
@@ -16,27 +22,29 @@ public class DataSet {
 	
 	public DataSet(String[] keys, Float[] values) {
 		this.data = new ArrayList<DataPair>();
-		try {
-			for (int i = 0; i < keys.length; i++) {
-				data.add(new DataPair(keys[i], values[i]));
-			}
-		} catch (ArrayIndexOutOfBoundsException aioobe) {
+		if (keys.length != values.length) {
 			try {
-				throw new KeyValueAmountException("Check your DataSet(s)!", aioobe);
-			} catch (KeyValueAmountException e) {
-				e.printStackTrace();
+				throw new KeyValueAmountException("Unequal amounts of keys and values!");
+			} catch (KeyValueAmountException ex) {
+				ExceptionWriter.write(ex);
 			}
+		}
+		for (int i = 0; i < keys.length; i++) {
+			data.add(new DataPair(keys[i], values[i]));
 		}
 	}
 	
-	public void setData(String[] keys, Float[] values) throws KeyValueAmountException {
+	public void setData(String[] keys, Float[] values) {
 		data.clear();
-		try {
-			for (int i = 0; i < keys.length; i++) {
-				data.add(new DataPair(keys[i], values[i]));
+		if (keys.length != values.length) {
+			try {
+				throw new KeyValueAmountException("Unequal amounts of keys and values!");
+			} catch (KeyValueAmountException ex) {
+				ExceptionWriter.write(ex);
 			}
-		} catch (ArrayIndexOutOfBoundsException aioobe) {
-			throw new KeyValueAmountException("Check your DataSet(s)!", aioobe);
+		}
+		for (int i = 0; i < keys.length; i++) {
+			data.add(new DataPair(keys[i], values[i]));
 		}
 	}
 	
@@ -44,7 +52,11 @@ public class DataSet {
 		return data;
 	}
 	
-	public BarChart toBarChart(float x, float y, float dx, float dy) {
+	public LineChart toLineChart(float dx, float dy) {
+		return new LineChart(dx, dy).setData(this);
+	}
+	
+	public BarChart toBarChart(float dx, float dy) {
 		return new BarChart(dx, dy).setData(this);
 	}
 	
@@ -52,4 +64,11 @@ public class DataSet {
 		return new PieChart(r).setData(this);
 	}
 	
+	public float getTotal() {
+		float total = 0;
+		for (DataPair pair : data) {
+			total += pair.value;
+		}
+		return total;
+	}
 }
