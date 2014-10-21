e425fd18e0f9fad261814ee15a77f2406ed2267d
TheMaxJ <maxjohnson@Maxs-MacBook-Air-3.local>
2014-10-21 11:19:50 -0700

    Documentation
    More Doc-Comments - Yay

--- src/me/max/chartly/charts/Chart.java
+++ src/me/max/chartly/charts/Chart.java
@@ -5,13 +5,42 @@ import me.max.chartly.components.data.DataSet;
 
 public interface Chart {
 
+	/**
+	 * Draws the graph
+	 * @param x Bottom left corner X-Coord
+	 * @param y Bottom left corner Y-Coord
+	 */
 	public void draw(float x, float y);
+	
+	/**
+	 * Re-Draws the graph
+	 */
 	public void refresh();
 	
+	/**
+	 * Provides the chart with its data
+	 * @param data DataSet to give the chart
+	 * @return this
+	 */
 	public Chart setData(DataSet data);
+	
+	/**
+	 * Get the chart's data
+	 * @return Chart's dataset
+	 */
 	public DataSet getData();
 	
+	/**
+	 * Get the current ColorScheme
+	 * @return the ColorScheme
+	 */
 	public ColorScheme getColorScheme();
-	public Chart setColorScheme(ColorScheme scheme);
 	
+	/**
+	 * Provides the chart with a colorscheme
+	 * @param scheme the new colorscheme
+	 * @return this
+	 */
+	public Chart setColorScheme(ColorScheme scheme);
+		
 }
