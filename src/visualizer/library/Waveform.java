ad43adab1f748a6779f11a29fbfd640cb747d74f
sherman464 <sherman464@gmail.com>
2014-10-27 11:48:27 -0700

    Fixed
    Now eq only makes one waveform. Also added the int y functionality to
    the second method.

--- src/visualizer/library/Waveform.java
+++ src/visualizer/library/Waveform.java
@@ -10,22 +10,18 @@ public class Waveform {
 	public void wf(){
 		Visualizer.Parent.stroke(0,255,0);
 		for(int i = 0; i < Visualizer.song.bufferSize() - 1;i++){
-			PApplet.println(Visualizer.song.left.get(i));
 			float x1 = PApplet.map(i, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
 			float x2 = PApplet.map(i+1, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
-			Visualizer.Parent.line(x1, 50 + Visualizer.song.left.get(i) * 50, x2, 50 + Visualizer.song.left.get(i + 1) * 50);
-			Visualizer.Parent.line(x1, 150 + Visualizer.song.right.get(i) * 50, x2, 150 + Visualizer.song.right.get(i + 1) * 50);
-		}
-	}
-	public void wf(int y){
-		Visualizer.Parent.stroke(0,255,0);
-		for(int i = 0; i < Visualizer.song.bufferSize() - 1;i++){
-			PApplet.println(Visualizer.song.left.get(i));
-			float x1 = PApplet.map(i, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
-			float x2 = PApplet.map(i+1, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
-			Visualizer.Parent.line(x1, 50 + Visualizer.song.left.get(i) * 50, x2, 50 + Visualizer.song.left.get(i + 1) * 50);
-			Visualizer.Parent.line(x1, 150 + Visualizer.song.right.get(i) * 50, x2, 150 + Visualizer.song.right.get(i + 1) * 50);
+			Visualizer.Parent.line(x1, 50 + Visualizer.song.mix.get(i) * 50, x2, 50 + Visualizer.song.left.get(i + 1) * 50);
 		}
 	}
 	
+	public void wf(int y){
+		Visualizer.Parent.stroke(0,255,0);
+		for(int i = 0; i < Visualizer.song.bufferSize() - 1;i++){
+			float x1 = PApplet.map(i, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
+			float x2 = PApplet.map(i+1, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
+			Visualizer.Parent.line(x1, y + Visualizer.song.mix.get(i) * 50, x2, y + Visualizer.song.left.get(i + 1) * 50);
+		}
+	}
 }
