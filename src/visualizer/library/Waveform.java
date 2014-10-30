f2bd2962c191950f9ef9f2d25e046e22ea1c7a4c
sherman464 <sherman464@gmail.com>
2014-10-30 11:50:36 -0700

    Add
    Added another waveform method

--- src/visualizer/library/Waveform.java
+++ src/visualizer/library/Waveform.java
@@ -16,7 +16,17 @@ public class Waveform {
 		}
 	}
 	
-	public void wf(int y){
+	public void wf(String channel){
+		Visualizer.Parent.stroke(0,255,0);
+		for(int i = 0; i < Visualizer.song.bufferSize() - 1;i++){
+			float x1 = PApplet.map(i, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
+			float x2 = PApplet.map(i+1, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
+			Visualizer.Parent.line(x1, 50 + Visualizer.song.mix.get(i) * 50, x2, 50 + Visualizer.song.left.get(i + 1) * 50);
+		}
+	}
+	
+	
+	public void wf(String channel, int y){
 		Visualizer.Parent.stroke(0,255,0);
 		for(int i = 0; i < Visualizer.song.bufferSize() - 1;i++){
 			float x1 = PApplet.map(i, 0, Visualizer.song.bufferSize(), 0, Visualizer.width);
