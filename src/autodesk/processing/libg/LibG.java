8b5491d19560aaf8ad891405ed0296f16d22e433
Patrick Tierney <patrick.tierney@autodesk.com>
2014-11-13 11:55:38 -0800

    Added some files

--- src/autodesk/processing/libg/LibG.java
+++ src/autodesk/processing/libg/LibG.java
@@ -25,10 +25,12 @@
  * @version     ##library.prettyVersion## (##library.version##)
  */
 
-package template.library;
-
+package autodesk.processing.libg;
 
 import processing.core.*;
+
+import autodesk.libg.*;
+import autodesk.libg.preloader.*;
 
 /**
  * This is a template class and can be used to start a new processing library or tool.
@@ -41,16 +43,12 @@ import processing.core.*;
  * @example Hello 
  */
 
-public class HelloLibrary {
-	
+public class LibG {
 	// myParent is a reference to the parent sketch
 	PApplet myParent;
-
-	int myVariable = 0;
 	
 	public final static String VERSION = "##library.prettyVersion##";
 	
-
 	/**
 	 * a Constructor, usually called in the setup() method in your sketch to
 	 * initialize and start the library.
@@ -58,8 +56,32 @@ public class HelloLibrary {
 	 * @example Hello
 	 * @param theParent
 	 */
-	public HelloLibrary(PApplet theParent) {
+	public LibG(PApplet theParent) {
 		myParent = theParent;
+		
+		System.out.println("About to loadDLL");
+		
+		try {
+			System.load("C:\\Users\\patrick\\git\\Autodesk.Processing.LibG\\lib\\LibGPreloader.dll");
+		} catch (UnsatisfiedLinkError error) {
+			System.out.println(error.toString());
+		}
+		
+		System.out.println("Loaded a DLL");
+
+		
+		//System.load("C:\\Users\\patrick\\git\\Autodesk.Processing.LibG\\lib\\msvcr100.dll");
+
+		//System.load("C:\\Users\\patrick\\git\\Autodesk.Processing.LibG\\lib\\LibGPreloader.dll");
+		
+		System.out.println("Loaded to DLL");
+
+		
+		//autodesk.libg.preloader.AsmPreloader.preload_libg_libraries("C:\\Users\\patrick\\git\\Autodesk.Processing.LibG\\liib");
+		//autodesk.libg.preloader.AsmPreloader.preload_asm_libraries("C:\\Program Files\\Autodesk\\Revit MEP 2016");
+
+		//autodesk.libg.LibG.start_asm_library();
+		
 		welcome();
 	}
 	
@@ -69,9 +91,6 @@ public class HelloLibrary {
 	}
 	
 	
-	public String sayHello() {
-		return "hello library.";
-	}
 	/**
 	 * return the version of the library.
 	 * 
@@ -81,23 +100,5 @@ public class HelloLibrary {
 		return VERSION;
 	}
 
-	/**
-	 * 
-	 * @param theA
-	 *          the width of test
-	 * @param theB
-	 *          the height of test
-	 */
-	public void setVariable(int theA, int theB) {
-		myVariable = theA + theB;
-	}
-
-	/**
-	 * 
-	 * @return int
-	 */
-	public int getVariable() {
-		return myVariable;
-	}
 }
 
