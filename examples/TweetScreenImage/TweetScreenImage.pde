30f341eaab276df3ecb2123459a16f30cf5b7ae9
dr <david@davidrueter.com>
2016-02-04 09:40:54 -0600

    Added image posting, plus example.

---
+++ examples/TweetScreenImage/TweetScreenImage.pde
@@ -0,0 +1,41 @@
+import com.davidrueter.twitter.*;
+import twitter4j.*;
+
+TwitterAPI api;
+int hue = 0;
+PGraphics g; // offscreen graphics object.
+
+/*
+  This example tweets what Processing is displaying.
+  Press the 't' key to tweet the image on the screen.
+*/
+
+void setup() {
+  size(480,480);
+    // Important -- replace the below strings with your own info!
+  api = new TwitterAPI(this, "consumerKey",
+                              "consumerSecret",
+                              "accessToken",
+                              "accessTokenSecret");
+  g = createGraphics(width, height); // initialize offscreen graphics
+}
+
+void keyPressed(){
+  if (key == 't'){ // Press t to tweet the screen
+    boolean success = api.tweetImage(g, "Twitter2.jpeg", "image from screen");  
+    if (success)  java.awt.Toolkit.getDefaultToolkit().beep();
+  }
+}
+
+void draw() {
+  // Draw to an offscreen buffer, g, so that we can send the image both to the screen and twitter.
+  g.beginDraw();
+  g.colorMode(HSB);
+  g.background(hue, 255, 200);
+  g.endDraw();
+  
+  // draw the image to the screen
+  image(g,0,0);
+  hue++;
+  if (hue > 255) hue = 0;
+}
--- src/com/davidrueter/twitter/TwitterAPI.java
+++ src/com/davidrueter/twitter/TwitterAPI.java
@@ -1,10 +1,23 @@
 package com.davidrueter.twitter;
 
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Iterator;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 import twitter4j.FilterQuery;
 import twitter4j.Status;
 import twitter4j.StatusAdapter;
@@ -15,8 +28,11 @@ import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.UserStreamAdapter;
+import twitter4j.auth.OAuthAuthorization;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.media.ImageUpload;
+import twitter4j.media.ImageUploadFactory;
 
 /**
  * Simplified interface to twitter4j for programming beginners.
@@ -26,16 +42,16 @@ import twitter4j.conf.ConfigurationBuilder;
  * 
  */
 public class TwitterAPI {
-	
+
 	// myParent is a reference to the parent sketch
 	PApplet myParent;
 
 	private Method statusMethod;
 	private Configuration twitterConfig;
-	
+
 	public final static String VERSION = "##library.prettyVersion##";
 	private Twitter twitter;
-	
+
 
 	/** Constructs a new Twitter API instance.
 	 * 
@@ -58,15 +74,15 @@ public class TwitterAPI {
 			String accessTokenSecret) {
 		myParent = theParent;
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		  cb.setOAuthConsumerKey(consumerKey);
-		  cb.setOAuthConsumerSecret(consumerSecret);
-		  cb.setOAuthAccessToken(accessToken);
-		  cb.setOAuthAccessTokenSecret(accessTokenSecret);
+		cb.setOAuthConsumerKey(consumerKey);
+		cb.setOAuthConsumerSecret(consumerSecret);
+		cb.setOAuthAccessToken(accessToken);
+		cb.setOAuthAccessTokenSecret(accessTokenSecret);
 		twitterConfig = cb.build();
 		twitter = new TwitterFactory(twitterConfig).getInstance();
-		  
+
 	}
-	
+
 	/** Filter all tweets, receiving current statuses that contain the supplied
 	 * search term. Multiple strings may be supplied. Do not include hashtags --
 	 * if you want to filter hashtags only, you'll have to exclude the # symbol
@@ -77,7 +93,7 @@ public class TwitterAPI {
 		TwitterStream twitterStream = setupFilterStream();
 		twitterStream.filter(filters);
 	}
-	
+
 	/** Start receiving status updates from users whose IDs are in
 	 * idsToFollow.
 	 * @param idsToFollow the list of IDs to follow
@@ -86,7 +102,7 @@ public class TwitterAPI {
 		FilterQuery q = new FilterQuery(idsToFollow);
 		filter(q);
 	}
-	
+
 	/** Filter an arbitrary FilterQuery. See Twitter4j docs for more details.
 	 * 
 	 * @param query
@@ -95,7 +111,7 @@ public class TwitterAPI {
 		TwitterStream twitterStream = setupFilterStream();
 		twitterStream.filter(query);
 	}
-	
+
 	/** Filter based on geographical location. Specify a bounding box, by
 	 * supplying the SW corner first (latitude, then longitude), then the NE corner.
 	 * 
@@ -109,7 +125,7 @@ public class TwitterAPI {
 		q.locations(new double[][]{{swLon, swLat},{neLon,neLat}});
 		filter(q);
 	}
-	
+
 	/** Receive all status updates made by a user.
 	 * FIXME FIXME FIXME
 	 */
@@ -121,30 +137,30 @@ public class TwitterAPI {
 			public void onStatus(Status status) {
 				dispatchStatus(status);
 			}
-			
+
 		};
 		TwitterStream twitterStream = makeStream();
 		twitterStream.addListener(adapter);
 		twitterStream.user();
-		
+
 	}
-	
-	
-	
+
+
+
 	protected TwitterStream setupFilterStream() {
 		initOnStatusCallback();
 
 		// Create a new status listener
-	  StatusListener listener = new StatusAdapter(){
-	    public void onStatus(Status status) {
-	       dispatchStatus(status); 
-	    }
-	    public void onException(Exception ex) {
-	        ex.printStackTrace();
-	    }
-	  };
-	  TwitterStream twitterStream = makeStream();
-	  twitterStream.addListener(listener);
+		StatusListener listener = new StatusAdapter(){
+			public void onStatus(Status status) {
+				dispatchStatus(status); 
+			}
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+		};
+		TwitterStream twitterStream = makeStream();
+		twitterStream.addListener(listener);
 		return twitterStream;
 	}
 
@@ -164,9 +180,10 @@ public class TwitterAPI {
 			logThrowable(e);
 		}
 	}
-	
+
 	protected void logThrowable(Throwable t){
-		t.printStackTrace();
+		System.err.println("Twitter API error encountered, details below:");
+		t.printStackTrace(System.err);
 	}
 
 	protected void initOnStatusCallback() {
@@ -176,7 +193,12 @@ public class TwitterAPI {
 			throw new RuntimeException("No onStatus() method found in the sketch.");
 		}
 	}
-	
+
+	/** Tweets the supplied status.
+	 * 
+	 * @param status the status to tweet
+	 * @return
+	 */
 	public boolean tweet(String status){
 		try {
 			twitter.updateStatus(status);
@@ -186,7 +208,118 @@ public class TwitterAPI {
 			return false;
 		}
 	}
-	
+
+	/** Tweets an image plus a status.
+	 * 
+	 * @param image the image to tweet
+	 * @param status the status to add
+	 * @param imageName a name for the image.
+	 * @return
+	 */
+	public boolean tweetImage( PImage image, String status, String imageName){
+		try {
+			return tweetImage(makeStreamFromPImage(image), imageName, status);
+		} catch (IOException e) {
+			logThrowable(e);
+			return false;
+		}
+	}
+
+	private ImageWriter imageioWriter(String extension) {
+		Iterator<ImageWriter> iter = ImageIO.getImageWritersByFormatName(extension);
+		if (iter.hasNext()) {
+			return iter.next();
+		}
+		return null;
+	}
+
+
+	/** Implementation copied from LGPL source in Processing's PImage class. Unfortunately,
+	 * the code wasn't factored to allow reuse :(
+	 * Currently, this implementation stores the image in memory as a JPEG, then sends it.
+	 * A future implementation might optimize this and start streaming the JPEG before it is fully
+	 * constructed.
+	 * @param image
+	 * @return
+	 */
+	public InputStream makeStreamFromPImage(PImage image) throws IOException {
+
+		int outputFormat = BufferedImage.TYPE_INT_RGB;
+
+		BufferedImage bimage = new BufferedImage(image.pixelWidth, image.pixelHeight, outputFormat);
+		bimage.setRGB(0, 0, image.pixelWidth, image.pixelHeight, image.pixels, 0, image.pixelWidth);
+
+		ImageWriter writer = null;
+		ImageWriteParam param = null;
+		IIOMetadata metadata = null;
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		if ((writer = imageioWriter("jpeg")) != null) {
+			// Set JPEG quality to 90% with baseline optimization. Setting this
+			// to 1 was a huge jump (about triple the size), so this seems good.
+			// Oddly, a smaller file size than Photoshop at 90%, but I suppose
+			// it's a completely different algorithm.
+			param = writer.getDefaultWriteParam();
+			param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+			param.setCompressionQuality(0.9f);
+
+			writer.setOutput(ImageIO.createImageOutputStream(out));
+			//		        writer.write(null, new IIOImage(bimage, null, null), param);
+			writer.write(metadata, new IIOImage(bimage, null, metadata), param);
+			writer.dispose();
+
+		} else {
+			boolean written = javax.imageio.ImageIO.write(bimage, "jpeg", out);
+			if (!written) return null;
+		}
+		return new ByteArrayInputStream(out.toByteArray());
+
+	}
+
+	/** Tweets the supplied image plus a status. Uses the same logic for resolving file
+	 * paths as Processing's loadImage and loadStrings functions.
+	 * 
+	 * @param imagePath path to the image (a local path relative to the data folder,
+	 * an absolute path, or a url.
+	 * @param imageName
+	 * @param status
+	 * @return
+	 */
+	public boolean tweetImage(String imagePath, String imageName, String status ){
+		InputStream in = myParent.createInput(imagePath);
+		return tweetImage(in, imageName, status);
+	}
+
+	/** Tweets the supplied image plus a status.
+	 * 
+	 * @param in
+	 * @param imageName
+	 * @param status
+	 * @return
+	 */
+	public boolean tweetImage(InputStream in, String imageName, String status) {
+		ImageUpload uploader = new ImageUploadFactory(twitterConfig)
+		.getInstance( new OAuthAuthorization(twitterConfig));	
+		try {
+			uploader.upload(imageName, in ,status);
+			return true;
+		} catch (TwitterException e) {
+			logThrowable(e);
+			return false;
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/** Gets an array of longs, representing the current users's followers.
+	 * 
+	 * @return a long array of foller IDs
+	 */
 	public long[] getFollowers(){
 		try {
 			return twitter.getFollowersIDs(twitter.getId()).getIDs();
