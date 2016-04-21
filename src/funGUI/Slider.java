668ffbd37218f995b315756bb485c2ed5c01e7a9
audreyseo <audreyleeseo@gmail.com>
2016-04-20 21:25:03 -0400

    Added new Slider code
    Updated Slider code allows the user to choose where the text appears relative t
    o the slider. Thinking of implementing something similar across the entire libr
    ary.

--- src/funGUI/Slider.java
+++ src/funGUI/Slider.java
@@ -17,6 +17,7 @@ public class Slider extends Frame implements PConstants {
 	protected float max;
 	String label;
 	PFont f;
+	Direction textPosition = Direction.UP;
 //	PGraphics g;
 //	PApplet p;
 	
@@ -103,6 +104,20 @@ public class Slider extends Frame implements PConstants {
 	}
 	
 	/**
+	 * Changes the position of the text to a new position for the text:
+	 * RIGHT, LEFT, UP, DOWN. All of these are relative to the slider.
+	 * @param newPosition		Direction, the new relative position of the slider.
+	 */
+	public void alignText(Direction newPosition) {
+		this.textPosition = newPosition;
+	}
+	
+	public void assignLowHigh(float low, float high) {
+		this.min = low;
+		this.max = high;
+	}
+	
+	/**
 	 * Displays the slider on the screen, fully operational.
 	 */
 	public void draw() {
@@ -116,11 +131,73 @@ public class Slider extends Frame implements PConstants {
 		if (label != null) {
 			g.pushStyle();
 			g.textFont(f);
+			
 			g.textAlign(CENTER, CENTER);
+			
 			g.fill(0);
-			g.text(label, x, y - 25);
+			g.text(label, x + xOffset(), y + yOffset());
 			g.popStyle();
 		}
+	}
+	
+	void customAlignText() {
+		int hAlign = CENTER;
+		int vAlign = CENTER;
+		
+		switch(textPosition) {
+		case LEFT:
+			hAlign = RIGHT;
+		break;
+		case RIGHT:
+			hAlign = LEFT;
+			break;
+		case UP: case DOWN:
+			hAlign = CENTER;
+			vAlign = CENTER;
+			break;
+		default:
+			break;
+		}
+		
+		g.textAlign(hAlign, vAlign);
+	}
+	
+	float xOffset() {
+		float dx = 0;
+		switch(textPosition) {
+		case LEFT:
+			dx = -.25f * w();
+			break;
+		case RIGHT:
+			dx = .25f * w();
+			break;
+		case DOWN: case UP:
+			dx = 0;
+			break;
+		default:
+			break;
+		}
+		
+		return(dx);
+	}
+	
+	float yOffset() {
+		float dy = 0;
+		switch(textPosition) {
+		case LEFT: case RIGHT:
+			dy = 0;
+			break;
+		case DOWN: 
+			dy = 25;
+			break;
+		case UP:
+			dy = -25;
+			break;
+		default:
+			break;
+		}
+		
+		return(dy * .5f);
 	}
 
 	void bg() {
@@ -128,47 +205,56 @@ public class Slider extends Frame implements PConstants {
 		g.rectMode(CENTER);
 		g.fill(255);
 		g.stroke(30);
-		g.rect(x, y, w, h);
+		
+		float yi = y - yOffset();
+		
+		g.rect(x - xOffset(), yi, w, h);
 		float dx = (w < 150) ? .48f : .49f;
-		g.arc((float) (x - w * dx), y, h, h, HALF_PI, HALF_PI * 3, OPEN);
-		g.arc((float) (x + w * .5), y, h, h, HALF_PI * 3, HALF_PI * 5, OPEN);
-		float xi = (float) (x - w * .45);
+		g.arc((float) (x - xOffset() - w * dx), yi, h, h, HALF_PI, HALF_PI * 3, OPEN);
+		g.arc((float) (x - xOffset() + w * dx), yi, h, h, HALF_PI * 3, HALF_PI * 5, OPEN);
+		
+		
+		float xi = (float) (x - xOffset() - w * .45);
 		int total = 10;
 		if (range() > 3) {
 			total = PApplet.round(range());
 		}
-		float step = (float) (((x + w * .45) - xi) / total);
+		float step = (float) (((x - xOffset() + w * .45) - xi) / total);
 		float exp = (float) Math.log10(total);
 		int apprxExp = Math.round(exp);
 		float a = (step < 10) ? 2.5f : 1f;
+		
 		if (apprxExp >= 2) {
 			step = (float) ((w * .9) / 10);
 			a = 1f;
 		}
+		
 		for (int i = 0; i < ((w * .9) / (a * step)) + 1; i++) {
 			g.fill(30);
 			g.strokeWeight((float) (1.5));
 			if (i % 2 == 1)
 				g.strokeWeight((float) (.5));
-			g.rect(xi + step * a * i, y, 2, 10);
+			g.rect(xi + step * a * i, yi, 2, 10);
 		}
+		
 		g.strokeWeight(3);
-		g.line(xi, y, xi + w * .9f, y);
+		g.line(xi, y - yOffset(), xi + w * .9f, yi);
 		g.popStyle();
 	}
 
 	void slider() {
 		g.pushStyle();
-		float xs = (float) (x - w * .45 + percent() * (w * .9));
+		float xs = (float) (x - xOffset() - w * .45 + percent() * (w * .9));
+		float ys = y - yOffset();
 		g.rectMode(CENTER);
 		g.fill(255);
 		g.stroke(0);
 		g.beginShape();
-		g.vertex((float) (xs - 3.5), (float) (y - 7.5));
-		g.vertex((float) (xs - 3.5), (float) (y + 5));
-		g.vertex(xs, (float) (y + 10));
-		g.vertex((float) (xs + 3.5), (float) (y + 5));
-		g.vertex((float) (xs + 3.5), (float) (y - 7.5));
+		g.vertex((float) (xs - 3.5), (float) (ys - 7.5));
+		g.vertex((float) (xs - 3.5), (float) (ys + 5));
+		g.vertex(xs, (float) (ys + 10));
+		g.vertex((float) (xs + 3.5), (float) (ys + 5));
+		g.vertex((float) (xs + 3.5), (float) (ys - 7.5));
 		g.endShape(CLOSE);
 		// g.rect(xs, y, 7, 15);
 		g.popStyle();
@@ -182,13 +268,13 @@ public class Slider extends Frame implements PConstants {
 //		float xs = (float) (x - w * .45 + percent() * (w * .9));
 		if (slid() || following) {
 			percent = PApplet.constrain(PApplet.map((float) (p.mouseX),
-					(float) (x - .45 * w), (float) (x + .45 * w), 0.0f, 1.0f),
+					(float) (x - xOffset() - .45 * w), (float) (x - xOffset() + .45 * w), 0.0f, 1.0f),
 					0.0f, 1.0f);
 		}
 		
 		if (!pressed && slid()) {
 			following = !following;
-		} else if ((!pressed && !slid() && PApplet.dist(p.mouseX, p.mouseY, x, y) > w * 1.5) || (!p.mousePressed)) {
+		} else if ((!pressed && !slid() && PApplet.dist(p.mouseX, p.mouseY, x - xOffset(), y - yOffset()) > w * 1.5) || (!p.mousePressed)) {
 			following = false;
 		}
 		
@@ -197,7 +283,7 @@ public class Slider extends Frame implements PConstants {
 	}
 
 	boolean slid() {
-		return (p.mousePressed && p.mouseX > x - .45 * w && p.mouseX < x + .45 * w && p.mouseY > y - 7.5 && p.mouseY < y + 7.5);
+		return (p.mousePressed && p.mouseX > x - xOffset() - .45 * w && p.mouseX < x - xOffset() + .45 * w && p.mouseY > y - yOffset() - 7.5 && p.mouseY < y - yOffset() + 7.5);
 	}
 	
 	/**
@@ -237,5 +323,23 @@ public class Slider extends Frame implements PConstants {
 	@Override
 	public String returnName() {
 		return("Slider");
+	}
+	
+	@Override
+	public float w() {
+		float w0 = w + h;
+		g.textFont(f);
+		float textwid = g.textWidth(label);
+		switch(textPosition) {
+		case UP: case DOWN:
+			w0 = (textwid < w0) ? w0 : textwid;
+			break;
+		case RIGHT: case LEFT:
+			w0 += 30 + textwid;
+			break;
+		default:
+			break;
+		}
+		return(w0);
 	}
 }
