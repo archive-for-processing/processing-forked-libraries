5ea3b97905e8485948ff718dc1e3f7a3c81abce3
KerstinJ <KerstinJ@192.168.1.43>
2017-12-13 19:36:46 +0100

    first upload of library

---
+++ src/mazeGenerator/MazeCreator.java
@@ -0,0 +1,14 @@
+package mazeGenerator;
+/**
+ * @author AmazingGroup 
+ * This class represents the Strategy interface. Which
+ * makes the maze class(context) independent on how the maze algorithms
+ * are implemented.
+ */
+
+public interface MazeCreator {
+	public void setUpStructure(Cell[][] mazeFields);
+
+	public void createMaze(Cell startingField, Cell endField);
+}
+
---
+++ src/mazeGenerator/RecursiveBacktracker.java
@@ -0,0 +1,30 @@
+package mazeGenerator;
+/** 
+ * This class implements the Recursive Backtracking algorithm for creating mazes.
+ * It creates a long winding path with few dead ends.
+ */
+
+import java.util.ArrayList;
+
+public class RecursiveBacktracker extends RecursiveStrategy {
+
+	public Cell handleCases(ArrayList<Cell> neighbours, Cell current) {
+
+		// if the cell has neighbours, that have not been visited yet
+		if (neighbours.size() > 0) {
+			return basicMode(neighbours, current);
+		}
+
+		else {
+			return huntMode(neighbours, current);
+		}
+	}
+
+	Cell huntMode(ArrayList<Cell> neighbours, Cell current) {
+		// System.out.println("Dead End!"); //debug
+		backTrackingStack.pop();
+		current = backTrackingStack.peek();
+		return current;
+	}
+
+}
