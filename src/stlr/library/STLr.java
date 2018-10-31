e5ceca84d06157b4f2cae175f5b6bdc9ff11f796
Tait Weicht <taitweicht@icloud.com>
2018-10-30 22:20:02 -0700

    Wrote feature to sheath cubic splines PShape curves in a PShape tube

--- src/stlr/library/STLr.java
+++ src/stlr/library/STLr.java
@@ -78,6 +78,143 @@ public class STLr implements PConstants{
 		return triangle;
 	}
 	
+	public PShape noodlize(PShape curve, float radius, int rGran, int lGran) {
+		if(curve.getVertexCount() < 4)
+			return null;
+		PVector[][] guide = controlSystem(controlPath(curve, lGran));
+		PVector[] ring1 = new PVector[rGran];
+		PVector[] ring2 = new PVector[rGran];
+		PVector[] temp;
+		PVector x, y, r1a, r1b, r2a, r2b;
+		//Initialize rings
+		for(int i = 0; i < rGran; i++) {
+			x = PVector.mult(guide[2][0], PApplet.cos(TWO_PI*i/rGran));
+			y = PVector.mult(guide[3][0], PApplet.sin(TWO_PI*i/rGran));
+			ring1[i] = PVector.add(guide[0][0], x).add(y);
+			ring2[i] = new PVector();
+		}
+		//Setup shape
+		PShape noodle = parent.createShape();
+		noodle.beginShape(TRIANGLES);
+		for(int i = 1; i < guide[0].length; i++) {
+			//Swap rings
+			temp = ring1; ring1 = ring2; ring2 = temp;
+			//Fill ring1
+			for(int j = 0; j < rGran; j++) {
+				x = PVector.mult(guide[2][i], PApplet.cos(TWO_PI*i/rGran));
+				y = PVector.mult(guide[3][i], PApplet.sin(TWO_PI*i/rGran));
+				ring1[i].set(guide[0][i]).add(x).add(y);
+			}
+			for(int j = 0; j < rGran; j++) {
+				r1a = ring1[j];
+				r1b = ring1[(j + 1) % rGran];
+				r2a = ring2[j];
+				r2b = ring2[(j + 1) % rGran];
+				//Triangle r1a-r1b-r2a
+				noodle.vertex(r1a.x, r1a.y, r1a.z);
+				noodle.vertex(r1b.x, r1b.y, r1b.z);
+				noodle.vertex(r2a.x, r2a.y, r2a.z);
+				//Triangle r2b-r2a-r1b
+				noodle.vertex(r2b.x, r2b.y, r2b.z);
+				noodle.vertex(r2a.x, r2a.y, r2a.z);
+				noodle.vertex(r1b.x, r1b.y, r1b.z);
+			}
+		}
+		noodle.endShape();
+		return noodle;
+	}
+	
+	private PVector[] controlPath(PShape curve, int lGran) {
+		PVector[] points = new PVector[(curve.getVertexCount() - 2)*lGran + 1];
+		PVector p0, p1, p2, p3;
+		p0 = curve.getVertex(0);
+		p1 = curve.getVertex(1);
+		p2 = curve.getVertex(2);
+		p3 = curve.getVertex(3);
+		int i;
+		for(i = 3; i < curve.getVertexCodeCount(); i++) {
+			p0.set(p1);
+			p1.set(p2);
+			p2.set(p3);
+			curve.getVertex(i, p3);
+			for(int j = 0; j < lGran; j++) {
+				points[(i - 3)*lGran + j] = catmullRom(p0, p1, p2, p3, (float)j/lGran);
+			}
+		}
+		//Last point is the last non-control point
+		points[points.length - 1] = curve.getVertex(i - 2);
+		return points;
+	}
+	
+	private PVector[][] controlSystem(PVector[] cp) {
+		//0 - position, 1 - tangent, 2 - binormal, 3 - normal
+		PVector[][] cs = new PVector[3][cp.length];
+		PVector[] tan = new PVector[cp.length - 1];
+		//Clone positions
+		cs[0] = cp;
+		//Create temporary tangents
+		for(int i = 0; i < tan.length; i++) {
+			tan[i] = PVector.sub(cp[i], cp[i + 1]).normalize();
+		}
+		//Derive binormal from tangents
+		for(int i = 1; i < cp.length - 1; i++) {
+			cs[2][i] = tan[i - 1].cross(tan[i]).normalize();
+		}
+		cs[2][0] = cs[2][1]; //Symmetric edge cases
+		cs[2][cs[0].length - 1] = cs[2][cs[0].length - 2];
+		//Use averaged tangents to preserve symmetry
+		//(reversed cp array should give same results)
+		for(int i = 1; i < cp.length - 1; i++) {
+			cs[1][i] = PVector.add(tan[i - 1], tan[i]).mult(0.5f);
+		}
+		cs[1][0] = tan[0]; //Edge cases
+		cs[1][cs[0].length - 1] = tan[tan.length - 1];
+		//Normals
+		for(int i = 0; i < cp.length; i++) {
+			cs[3][i] = cs[1][i].cross(cs[2][i]).normalize();
+		}
+		return cs;
+	}
+	
+	public float length(PShape curve, int grain) {
+		if(curve.getVertexCodeCount() < 4)
+			return 0;
+		float len = 0;
+		PVector p0, p1, p2, p3, previous, next;
+		p0 = new PVector();
+		p1 = curve.getVertex(0);
+		p2 = curve.getVertex(1);
+		p3 = curve.getVertex(2);
+		previous = new PVector();
+		next = new PVector();
+		int i;
+		for(i = 3; i < curve.getVertexCodeCount(); i++) {
+			p0.set(p1);
+			p1.set(p2);
+			p2.set(p3);
+			curve.getVertex(i, p3);
+			previous.set(p1);
+			for(int j = 1; j < grain; j++) {
+				next.set(catmullRom(p0, p1, p2, p3, (float)j/grain));
+				len += next.dist(previous);
+				previous.set(next);
+			}
+		}
+		next.set(curve.getVertex(i - 1));
+		len += next.dist(previous);
+		return len;
+	}
+	
+	public PVector catmullRom(PVector p0, PVector p1, PVector p2, PVector p3, float t) {
+		PVector tan1 = PVector.sub(p2, p0).mult(0.5f);
+		PVector tan2 = PVector.sub(p3, p1).mult(0.5f);
+		PVector interp = PVector.mult(tan1, t*(1 - t)*(1 - t));
+		interp.add(PVector.mult(tan2, t*t*(t - 1)));
+		interp.add(PVector.mult(p1, (1 + 2*t)*(1 - t)*(1 - t)));
+		interp.add(PVector.mult(p2, t*t*(3 - 2*t)));
+		return interp;
+	}
+	
 	public void generateBinarySTL(PShape obj, String name) {
 		
 		//TODO: I think this fails due to the first octant rule (all positive numbers)
